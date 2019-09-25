29d65eaa01e70f2dad74021eb2fe4cd2f598f678
dinhnguyenland <53521538+dinhnguyenland@users.noreply.github.com>
2019-09-25 17:58:04 +0700

    Update faceseo-form.js

--- faceseo-form.js
+++ faceseo-form.js
@@ -140,7 +140,7 @@ jQuery("#myModalthanh").html(formdiv);
 function thanhxet(){
 
 if(submittedform) {
-	window.location="https://dinhnguyenland.blogspot.com/s";
+	window.location="https://nhadatdautu247.com/";
 	
 	 if(document.cookie.indexOf("adf") == -1)
                    {
